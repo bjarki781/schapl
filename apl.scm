e0ce8831fdf6b4e2c7cade9eb4ad78f4890c7ce3
bjarki781 <bjg60@hi.is>
2020-08-21 13:28:15 +0000

    negative numbers, more operators, only hash notation

--- apl.scm
+++ apl.scm
@@ -1,4 +1,15 @@
+;;; SchAPL - simple APL interpreter written in Scheme
+
+; todo
+; * redo lexer, define better what is valid syntax
+;   (e.g. '# plus' and '3 a b c' is a valid now)
+; * implement more monadic and dyadic operators
+; * implement reduce and scan operators
+; * implement matrices and some operators
+
 (define (error str) (begin (display str) '()))
+(define (swap op) (lambda (a b) (op b a)))
+(define (curry op a) (lambda (b) (op a b)))
 (define (string-car str) (string-ref str 0))
 (define (string-cdr str) (string-tail str 1))
 (define (first-digit nustr) (char->digit (string-car nustr)))
@@ -9,30 +20,18 @@
 
 (define (array? str) (first-digit str))
 
-(define (unary? opstr)
-  (or
-    (equal? opstr "+")
-    (equal? opstr "-")
-    (equal? opstr "?")
-    (equal? opstr "!")))
-
-(define (binary? opstr)
-  (or
-    (equal? opstr "+")
-    (equal? opstr "-")
-    (equal? opstr ",")
-    (equal? opstr "/")))
-
 (define (reader str)
-  (if (first-digit str) 
+  (if (array? str) 
     (read-array str)
     (read-op str)))
 
 (define (read-array str) 
-  (string-take-until str (string->char-set "+-*/")))
+  (string-take-until str (string->char-set "#)")))
 
 (define (read-op str) 
-  (string-take-until str char-set:numeric))
+  (if (char=? (string-car str) #\#)
+      (string-append "#" (string-take-until (string-cdr str) (char-set-union char-set:numeric (string->char-set "#)")))) 
+      (error "operator must be begin with hash sign (#)")))
 
 (define (lex expr)
   (if (string-null? expr) 
@@ -41,10 +40,13 @@
       (cons (string-trim tok) (lex (string-tail expr (string-length tok)))))))
 
 (define (parse-number nustr)
-  (if (string-null? nustr)
-    0
+  (cond 
+    ((string-null? nustr) 0)
+    ((char=? (string-car nustr) #\-) (* -1 (parse-number (string-cdr nustr))))
+    ((char=? (string-car nustr) #\+) (parse-number (string-cdr nustr)))
+    (else 
     (+ (* (expt 10 (- (string-length nustr) 1)) (first-digit nustr))
-       (parse-number (string-cdr nustr)))))
+       (parse-number (string-cdr nustr)))))) 
 
 (define (tokenize-array arrstr)
   (if (string-null? arrstr)
@@ -61,22 +63,62 @@
     ((= (length arg1) (length arg2)) 
      (cons (op (car arg1) (car arg2))
            (apply-binary op (cdr arg1) (cdr arg2))))
-    (else (error "vectors not the same length"))))
+    ((= (length arg1) 1) (map (curry op (car arg1)) arg2))
+    ((= (length arg2) 1) (map (curry (swap op) (car arg2)) arg1))
+    (else (error "vectors must be as long or one a singlet"))))
 
 (define (parse-op opstr)
+  (if (not (equal? (string-car opstr) #\#)) 
+    (error "invalid operator")
+    (symbol-append 'op- (string->symbol (string-cdr opstr))))) 
+
+(define (signum a) 
   (cond
-    ((equal? opstr "+") 'op-add)
-    ((equal? opstr "-") 'op-sub)
-    ((equal? opstr "*") 'op-mul)
-    ((equal? opstr "!") 'op-exclemation)
-    ((equal? opstr "/") 'op-compression)))
+    ((< a 0) -1)
+    ((= a 0) 0)
+    ((> a 0) 1)
+    )) 
 
+(define (fac n)
+  (if (< n 2) 1 (* n (fac (- n 1)))))
 
-(define (op-add . args)
+(define (op-plus . args)
   (cond
     ((= (length args) 1) (car args))
     ((= (length args) 2) (apply-binary + (first args) (second args)))
     (else (error "add error"))))
+
+(define (op-minus . args)
+  (cond
+    ((= (length args) 1) (map (curry * -1) (first args)))
+    ((= (length args) 2) (apply-binary - (first args) (second args)))
+    (else (error "sub error"))))
+
+(define (op-multiply . args)
+  (cond
+    ((= (length args) 1) (map signum (first args)))
+    ((= (length args) 2) (apply-binary * (first args) (second args)))
+    (else (error "mul error"))))
+
+(define (op-divide . args)
+  (cond
+    ((= (length args) 1) (map (curry / 1) (first args)))
+    ((= (length args) 2) (apply-binary / (first args) (second args)))))
+
+(define (op-bang . args)
+  (cond 
+    ((= (length args) 1) (map fac (first args)))
+    ((= (length args) 2) (error "implement nCr"))
+    )
+  )
+
+(define (iota a b) 
+  (if (= a b) (list b) (cons a (iota (+ a 1) b))))
+
+(define (op-iota . args)
+  (cond 
+    ((= (length args) 1) (iota 1 (car (first args))))
+    (else (error "iota: too many arguments"))))
 
 (define (print-array array)
   (if (null? array) 
@@ -90,11 +132,11 @@
   (cond
     ((null? lexemes) '())
     ((= (length lexemes) 1) 
-      (list 'quote (parse-array (car lexemes))))
-    ((array? (car lexemes)) ; binary
-      (list (parse-op (cadr lexemes)) (parse (list-head lexemes 1)) (parse (cddr lexemes))))
+      (list 'quote (parse-array (first lexemes))))
+    ((array? (first lexemes)) ; binary
+      (list (parse-op (second lexemes)) (parse (list-head lexemes 1)) (parse (cddr lexemes))))
     (else ; unary
-      (list (parse-op (car lexemes)) (parse (cdr lexemes))))))
+      (list (parse-op (first lexemes)) (parse (cdr lexemes))))))
 
 (define environ (the-environment))
 
